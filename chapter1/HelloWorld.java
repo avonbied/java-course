76686bc47275573cf82afa500e1490cc230555b8
Lex <58815784+avonbied@users.noreply.github.com>
2019-01-18 12:45:35 -0600

    Day One

---
+++ chapter1/HelloWorld.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author lexvonbon
+ * @version 0.1
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+
+    public static void main() {
+        
+        System.out.println("Hello World!");
+        
+        System.out.print(1+2);
+        
+        System.out.print("1+2");
+        
+        System.out.print("");
+
+    }
+
+}
