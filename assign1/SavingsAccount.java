bc84e589260c86d3c79532dfb82051eda92fc86a
Lex <58815784+avonbied@users.noreply.github.com>
2019-02-14 17:24:51 -0600

    Create SavingsAccount.java

---
+++ assign1/SavingsAccount.java
@@ -0,0 +1,18 @@
+class SavingsAccount {
+  private double balance;
+
+  public SavingsAccount(double bal) {
+    setBalance(bal);
+  }
+
+  public void setBalance(double bal) {
+    balance = bal;
+  }
+  public double getBalance() {
+    return(balance);
+  }
+
+  public void addInterest(double i) {
+    balance += balance*i;
+  }
+}
