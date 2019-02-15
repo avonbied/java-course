57acd58cebd5122657daa9d84862add609138125
Lex <58815784+avonbied@users.noreply.github.com>
2019-02-15 00:32:48 -0600

    Suck my Ass

---
+++ pseudo1/SavingsAccount.java
@@ -0,0 +1,31 @@
+/*
+ * SavingsAccount Class
+ * 
+ * @author <author>
+ * @version <version>
+ * 
+ */
+
+ class SavingsAccount {
+  // Protected Balance
+
+  /* Constructor
+   * input:  double
+   * output: none
+  */
+
+  /* setBalance
+   * input:  double
+   * output: none
+  */
+  
+  /* getBalance
+   * input:  none
+   * output: double
+  */
+
+  /* addInterest
+   * input:  double
+   * output: none
+  */
+}
---
+++ pseudo1/SavingsAccountTester.java
@@ -0,0 +1,19 @@
+public class SavingsAccountTester {
+
+  public static void main(String[] args) throws IOException {
+
+    // File with info.txt in Constructor
+    // Scanner1 with File in Constructor
+
+    /* loop while File has next line
+      // Scanner2 with Scanner1 next line in Constructor
+      // Scanner2 with "," delimiter
+
+      * loop while Scanner2 has a next double
+        // SavingsAccount1 Scanner2 next double in Constructor
+        // SavingsAccount1 addInterest of Scanner2 next double
+        // Print line "My savings balance with interest is " followed by SavingsAccount1 balance
+      *
+    */
+  }
+}
