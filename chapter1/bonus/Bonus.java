9b3176ec9d4f9f7bf43a188493c6bec9cf8237a5
Lex <58815784+avonbied@users.noreply.github.com>
2019-02-15 12:40:27 -0600

    Kiss my Ass3

---
+++ chapter1/bonus/Bonus.java
@@ -0,0 +1,6 @@
+import java.awt.Graphics2D;
+class HouseTester {
+    public static void draw(Graphics2D g2) {
+        
+    }
+}
---
+++ chapter1/bonus/House.java
@@ -0,0 +1,4 @@
+import java.awt.Graphics2D;
+class House extends Graphics2D {
+    
+}
