cb42a3a729971b96789946c96d283fe2e70eb8e4
Lex <58815784+avonbied@users.noreply.github.com>
2019-02-22 12:26:51 -0600

    Kiss Ass 3.1

---
+++ section2/assign/Curfew.java
@@ -0,0 +1,8 @@
+/*Problem Statement I
+Calculating curfew level of a user to attend a party.
+
+Ask the user to enter their age. If the user is above the age of 13, then can go to the party. Otherwise they can't go.
+
+If the user's age is below 17, he/she have a curfew of 10:00 pm to retun home.
+
+Otherwise, if the user's age is above 16, the curfew is midnight.*/
---
+++ section2/assign/SumAndAverage.java
@@ -0,0 +1,19 @@
+/*Problem Statement II
+Write a program called SumAndAverage to produce the sum of 1, 2, 3, ..., to 100.
+Also compute and display the average.
+The output shall look like:
+
+The sum is 5050
+The average is 50.5*/
+class SumAndAverage {
+  static int sum;
+  static double avg;
+  public static void main(String[] args) {
+    for(int i = 0; i < 101; i += 1) {
+      sum += i;
+    }
+    avg = sum / 100;
+    System.out.println("The sum is "+sum);
+    System.out.println("The average is "+avg);
+  }
+}
